dae39ca35210b7d2a442dd899a8d20b305fc9ef2
Buttarelli Lucas <buttarelli.lucas@gmail.com>
2024-11-23 16:37:16 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Bienvenido a la Financiera</h1>
+      <p>Esta es la base del proyecto.</p>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/assets/Login.jsx
@@ -0,0 +1,15 @@
+const Login = () => {
+    const handleLogin = () => {
+      alert("Simulación de login");
+    };
+  
+    return (
+      <div>
+        <h2>Iniciar Sesión</h2>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    );
+  };
+  
+  export default Login;
+  
