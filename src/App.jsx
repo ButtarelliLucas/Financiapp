2b2d9964801fec48ac40125af17cff3d9d62777b
Buttarelli Lucas <buttarelli.lucas@gmail.com>
2024-11-23 16:40:25 -0300

    New Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import Login from "./components/Login.jsx";
+
 function App() {
   return (
     <div>
       <h1>Bienvenido a la Financiera</h1>
-      <p>Esta es la base del proyecto.</p>
+      <Login />
     </div>
   );
 }
 
 export default App;
-
